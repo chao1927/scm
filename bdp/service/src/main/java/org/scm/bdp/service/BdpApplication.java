8f3519fcbc4fcc6ac6bd31b558bf36bfedb457de
chaobo <chaobo@zjitd.com>
2025-04-21 13:24:29 +0800

    1. 基础资料平台管理开发

---
+++ bdp/service/src/main/java/org/scm/bdp/service/BdpApplication.java
@@ -0,0 +1,13 @@
+package org.scm.bdp.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdpApplication.class, args);
+    }
+
+}
